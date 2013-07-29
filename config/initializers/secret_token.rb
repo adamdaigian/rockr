a698c8bc479d7ac2c06fd7ccb18def598c989ae5
Adam Daigian <adam.daigian@gmail.com>
2013-07-29 15:09:09 -0700

    basic rails app with gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockr::Application.config.secret_token = 'ffc5343020e310d147dccf1aeefb99abd925e547a748378e1ccbf1dbfad408e549b99afb9f1c26d680f56fd104733714fd89c88882748941f61e76092e41cc7f'
